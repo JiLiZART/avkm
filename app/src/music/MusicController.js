11a691e6dbdc7d4ae058be55da32ab3ee74c62cf
Nikolay Kostyurin <jilizart@gmail.com>
2015-06-23 02:13:37 +0700

    music playlist row status icon

--- app/src/music/MusicController.js
+++ app/src/music/MusicController.js
@@ -74,8 +74,8 @@
         this.currentPosition = null;
         this.currentState = null;
 
-        this.onPlayerReady = function (PlayerAPI) {
-            self.playerAPI = PlayerAPI;
+        this.onPlayerReady = function (API) {
+            self.playerAPI = API;
         };
 
         this.onPlayerComplete = function() {
@@ -91,19 +91,24 @@
         };
 
         this.playByPosition = function(index) {
-            self.playerAPI.stop();
-            self.currentPosition = index;
-            self.currentSource = [self.audios[index]];
-            self.currentAudio = self.audios[index];
-            $timeout(self.playerAPI.play.bind(self.playerAPI), 100);
-        };
+            if (self.currentPosition === index) {
+                if (self.playerAPI.currentState === 'play') {
+                    self.playerAPI.pause();
+                }
 
-        this.playAudio = function () {
-            this.playByPosition(self.currentPosition);
+                if (self.playerAPI.currentState === 'pause') {
+                    self.playerAPI.play();
+                }
+            } else {
+                self.playerAPI.stop();
+                self.currentPosition = index;
+                self.currentSource = [self.audios[index]];
+                self.currentAudio = self.audios[index];
+                $timeout(self.playerAPI.play.bind(self.playerAPI), 100);
+            }
         };
 
         this.playGroup = function (audios, name, $event) {
-            self.currentPosition = 0;
             self.audiosName = name;
             self.audios = audios.map(function(audio) {
                 return {
@@ -117,7 +122,7 @@
                 };
             });
 
-            this.playByPosition(self.currentPosition);
+            this.playByPosition(0);
         };
 
         function loadFromAll() {
